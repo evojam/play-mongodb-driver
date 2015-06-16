9fafe7832eb5b28b7d38e022cc362555716b5176
Artur Bańkowski <artur.bankowski@gmail.com>
2015-06-16 10:16:09 +0200

    Fix handling of failure during reading
    Change-Id: Ia8d84b27b36fc07a6fc1a10f6fc3caf70b7efba3

--- src/main/scala/com/evojam/mongodb/play/json/Codec.scala
+++ src/main/scala/com/evojam/mongodb/play/json/Codec.scala
@@ -2,12 +2,10 @@ package com.evojam.mongodb.play.json
 
 import play.api.libs.json._
 
-import org.bson.codecs.configuration.CodecRegistries
-
 import com.evojam.mongodb.client.MongoClientSettings
-import com.evojam.mongodb.client.codec.Reader
-import com.evojam.mongodb.client.codec.Writer
+import com.evojam.mongodb.client.codec.{ Reader, Writer }
 import com.evojam.mongodb.play.json.codec._
+import org.bson.codecs.configuration.CodecRegistries
 
 object Codec {
   val registry =
@@ -20,14 +18,20 @@ object Codec {
   implicit lazy val jsObjectCodec = new JsObjectCodec(registry)
 
   implicit def jsValueReader[T: Reads]: Reader[T] = new Reader[T] {
+
     override type R = JsValue
     override val codec = jsValueCodec
-    override def read(doc: JsValue): T = Json.fromJson(doc).get
+
+    override def read(doc: JsValue): T =
+      doc.validate[T]
+        .recoverTotal(e =>
+          throw new Exception(s"Failed to decode json=$doc in reader ${this.getClass}  error=${e.toString}"))
   }
 
   implicit def jsValueWriter[T: Writes]: Writer[T] = new Writer[T] {
     override type R = JsValue
     override val codec = jsValueCodec
+
     override def write(doc: T): JsValue = Json.toJson(doc)
   }
 }
